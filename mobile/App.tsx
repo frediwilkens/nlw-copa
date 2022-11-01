a6719b072d287897835b3fdc82471289ba206a5b
Fredi Wilkens <frediwilkens@gmail.com>
2022-11-01 15:10:52 -0300

    feat: setup mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider, Text, Center } from 'native-base';
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} bgColor="black">
+        <Text color="white" fontSize={24}>
+          Hello React Native!
+        </Text>
+        <StatusBar style="auto" />
+      </Center>
+    </NativeBaseProvider>
+  );
+}
