bce04bb4270422484b7b6a6db8711652bfa3cb13
Fredi Wilkens <frediwilkens@gmail.com>
2022-11-01 15:13:52 -0300

    feat: customize a native base theme

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Text, Center } from 'native-base';
+import { THEME } from './src/styles/theme';
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
         <Text color="white" fontSize={24}>
           Hello React Native!
         </Text>
