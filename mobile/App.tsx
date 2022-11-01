802bb1699e6e2252e138e780d464aced4356549e
Fredi Wilkens <frediwilkens@gmail.com>
2022-11-01 15:39:09 -0300

    feat: add fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Text, Center } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/SignIn';
+
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24}>
-          Hello React Native!
-        </Text>
-        <StatusBar style="auto" />
-      </Center>
+        {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
