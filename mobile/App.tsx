779db251f32c6f2d61d6a787ce864a763aa8c526
Fredi Wilkens <frediwilkens@gmail.com>
2022-11-05 14:26:28 -0300

    feat(mobile): implement navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@
 import { AuthContextProvider } from './src/contexts/AuthContext';
 
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 
 import { THEME } from './src/styles/theme';
 
@@ -20,7 +20,7 @@ export default function App() {
           translucent
         />
 
-          {fontsLoaded ? <SignIn /> : <Loading />}
+          {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
